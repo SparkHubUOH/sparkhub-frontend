598c85b8944ce5650d056a63cbed606d334e8098
unknown <alaas.alamir@gmail.com>
2026-02-24 02:27:37 +0300

    project arrangement and i18n initializaion

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { TranslateService } from '../../services/translation/translation';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header  {
+  currentLang = 'en';
 
+  constructor(private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
+  toggleLanguage() {
+    this.currentLang = this.currentLang === 'en' ? 'ar' : 'en';
+    this.translate.use(this.currentLang);
+
+    document.documentElement.dir =
+      this.currentLang === 'ar' ? 'rtl' : 'ltr';
+  }
 }
+
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,45 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { APP_INITIALIZER, importProvidersFrom } from '@angular/core';
+import { TranslateService } from './app/services/translation/translation';
+import { 
+    TranslateHttpLoader, 
+    TRANSLATE_HTTP_LOADER_CONFIG 
+} from '@ngx-translate/http-loader'; 
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+    TranslateService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (ts: TranslateService) => () => (ts as any).initLanguage?.(),
+      deps: [TranslateService],
+      multi: true
+    },
+    {
+      provide: TRANSLATE_HTTP_LOADER_CONFIG,
+      useValue: {
+        prefix: './assets/i18n/', 
+        suffix: '.json'
+      }
+    },
+
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useClass: TranslateHttpLoader,
+          deps: [HttpClient]
+        },
+        defaultLanguage: 'en', 
+      }),
+    ),
+  ]
+}).catch(err => console.error(err));
